b33cfd2af9a85a6c24a089931b0656ccadc7cfc1
UnityPrac <3312422719@qq.com>
2019-05-17 13:56:33 +0800

    190426_190510答案

---
+++ 190511_190517/solution/Fire/Assets/Script/Fire2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire2 : FireBase {
+    // Use this for initialization
+    void Start () {
+        speed = new Vector3(-10, 0, 0);
+        destroyDistance = 50f;
+    }
+
+    public override void Fire() {
+        foreach (Transform child in transform) {
+            child.Translate(speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.U)) {
+            var bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            bullet.transform.position = transform.position;
+            bullet.transform.localScale *= 0.3f;
+            var body = bullet.AddComponent<Rigidbody>();
+            body.detectCollisions = false;
+            bullet.transform.parent = transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Fire();
+        Destroy();
+	}
+}
---
+++ 190511_190517/solution/Fire/Assets/Script/FireBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBase : MonoBehaviour {
+    protected Vector3 speed;
+    protected float destroyDistance;
+
+    public virtual void Fire() {
+        Debug.Log("Fire");
+    }
+
+    protected void Destroy() {
+        for (int i = 0; i < transform.childCount; i++) {
+            var bullet = transform.GetChild(i);
+            if ((bullet.position - transform.position).magnitude > destroyDistance) {
+                Destroy(bullet.gameObject);
+            }
+        }
+    }
+}
