1b5d42c3ea4360ba2827992b0cf2989aff830a53
UnityPrac <3312422719@qq.com>
2019-05-25 15:50:08 +0800

    190525_190531期作业

---
+++ 190525_190531/project/HelloWord/Assets/HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Log();
+	}
+
+    void Log() {
+        if (/* 修改这里 */) {
+            Debug.Log("Hello");
+        } else {
+            Debug.Log("World");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
