40d7d7eebd17ed32692e416b54977bfa8bfe4b50
UnityPrac <3312422719@qq.com>
2019-05-31 17:48:23 +0800

    190525_190531期作业答案

---
+++ 190525_190531/solution/HelloWord/Assets/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Log();
+	}
+
+    void Log() {
+        // if (new Func<bool>(() => { Debug.Log("Hello"); return false; }).Invoke()){
+        // if (Debug.Log("Hello") is object) {
+        if (true) Debug.Log("Hello"); if (false) {
+            Debug.Log("Hello");
+        } else {
+            Debug.Log("World");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
