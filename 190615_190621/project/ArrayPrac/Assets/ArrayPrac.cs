d88f07a0bc4e7f856e5cf4618e17f7e67e8a87f3
UnityPrac <3312422719@qq.com>
2019-06-14 17:06:33 +0800

    190615_190621作业

---
+++ 190615_190621/project/ArrayPrac/Assets/ArrayPrac.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayPrac : MonoBehaviour {
+    public int[] NumberArray = new int[10];
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
