c56b33c58a09bed5293b6556e6f50ff8536c9f01
UnityPrac <3312422719@qq.com>
2019-06-28 15:54:48 +0800

    190622_190628期作业答案

---
+++ 190622_190628/solution/SimpleCaculator/Assets/SimpleCaculator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleCaculator : MonoBehaviour {
+    public InputField Num1Field;
+    public InputField Num2Field;
+    public Dropdown OperatorDropdown;
+    public Text ResultText;
+
+    public void Calculate() {
+        string operation = OperatorDropdown.options[OperatorDropdown.value].text;
+        switch (operation) {
+            case "+":
+                ResultText.text = (float.Parse(Num1Field.text) + float.Parse(Num2Field.text)).ToString();
+                break;
+            case "-":
+                ResultText.text = (float.Parse(Num1Field.text) - float.Parse(Num2Field.text)).ToString();
+                break;
+            case "*":
+                ResultText.text = (float.Parse(Num1Field.text) * float.Parse(Num2Field.text)).ToString();
+                break;
+            case "/":
+                ResultText.text = float.Parse(Num2Field.text) == 0 ? "undefined" :
+                    (float.Parse(Num1Field.text) / float.Parse(Num2Field.text)).ToString();
+                break;
+            default:
+                break;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
